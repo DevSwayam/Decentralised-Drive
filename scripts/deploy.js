c7b797716c60c17637afaf3ab992b1ad7051f689
Swayam rajkumar karle <99035115+DevSwayam@users.noreply.github.com>
2023-01-18 22:56:57 +0530

    Setted path variables andcontracts path

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -7,20 +7,14 @@
 const hre = require("hardhat");
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
-  const unlockTime = currentTimestampInSeconds + ONE_YEAR_IN_SECS;
 
-  const lockedAmount = hre.ethers.utils.parseEther("1");
+  const Upload = await hre.ethers.getContractFactory("Upload");
+  const upload = await Upload.deploy();
 
-  const Lock = await hre.ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
+  await upload.deployed();
 
-  await lock.deployed();
-
-  console.log(
-    `Lock with 1 ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`
-  );
+  console.log("Contract Uploaded to address " , upload.address);
+ 
 }
 
 // We recommend this pattern to be able to use async/await everywhere
